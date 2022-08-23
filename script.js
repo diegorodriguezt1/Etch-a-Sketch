7a11a02c79cd920910e479b19ace8e263a41bab2
Diego Rodriguez <diegorodrigueztandil1@gmail.com>
2022-08-23 17:52:12 -0300

    Added a forEach to target all the cubes with the Event Listener and change de background color

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container")
 
 for (j = 0; j < 16; ++j){
     const vertical = document.createElement("div")
-    vertical.classList.add("line")
+    vertical.classList.add("column")
     container.append(vertical)
     for (i = 0; i < 16; ++i){
         const horizontal = document.createElement("div");
@@ -10,3 +10,12 @@ for (j = 0; j < 16; ++j){
         vertical.append(horizontal)
     }
 }
+
+const cubes = document.querySelectorAll(".cube")
+
+cubes.forEach(cube => {
+    cube.addEventListener("mouseover", () =>
+    cube.style.backgroundColor = "blue")
+})
+
+
