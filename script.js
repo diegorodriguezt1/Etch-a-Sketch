66891ef9d76a10d4407d4100a11493ec79973ef1
Diego Rodriguez <diegorodrigueztandil1@gmail.com>
2022-09-15 16:42:02 -0300

    Created the function to make the grid and added inside the event listener to paint the cubes. Also made a variable that contain the slider value to divide the container in equal parts

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const container = document.querySelector(".container")
+const slider = document.querySelector(".slider")
+const output = document.querySelector(".value")
+const paint = document.querySelector(".paint")
 
-for (j = 0; j < 16; ++j){
-    const vertical = document.createElement("div")
-    vertical.classList.add("column")
-    container.append(vertical)
-    for (i = 0; i < 16; ++i){
-        const horizontal = document.createElement("div");
-        horizontal.classList.add("cube")
-        vertical.append(horizontal)
-    }
+output.textContent = slider.value;
+
+slider.oninput = function() {
+    output.textContent = this.value;
 }
 
-const cubes = document.querySelectorAll(".cube")
+function createGrid() {
+    for (j = 0; j < slider.value; ++j){
+        const vertical = document.createElement("div")
+        vertical.classList.add("column")
+        container.append(vertical)
+        for (i = 0; i < slider.value; ++i){
+            const horizontal = document.createElement("div");
+            horizontal.classList.add("cube")
+            vertical.append(horizontal)
+        }
+    }
+    const cubes = document.querySelectorAll(".cube");
+    cubes.forEach(cube => {
+        cube.addEventListener("mouseover", () =>
+        cube.style.backgroundColor = paint.value)
+    })
+    let rootElement = document.documentElement;
+    let sliderValue = slider.value;
+    rootElement.style.setProperty("--sliderValue", sliderValue);
+}
 
-cubes.forEach(cube => {
-    cube.addEventListener("mouseover", () =>
-    cube.style.backgroundColor = "blue")
-})
-
+createGrid();
 
